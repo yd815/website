9687a0ed72b15056b55f8ad7951fd7b0adf15d4c
yd815 <yifan.dong0815@gmail.com>
2023-12-16 18:22:32 -0500

    fix error

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
 window.index = [{
     "title": "article 1",
-    "route": "../data/1.txt" 
+    "route": "data/1.txt" 
 },{
     "title": "article 2",
-    "route": "../data/2.txt" 
+    "route": "data/2.txt" 
 },
 ]
